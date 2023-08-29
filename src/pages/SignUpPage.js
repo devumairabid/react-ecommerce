c993c5411c0c48d25e80cf413a85f9facefc775d
Umair <devumairabid01@gmail.com>
2023-08-29 00:37:53 -0700

    first

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignUp from '../features/auth/components/SignUp'
+const SignUpPage = () => {
+    return (
+        <div>
+            <SignUp></SignUp>
+        </div>
+    )
+}
+
+export default SignUpPage
