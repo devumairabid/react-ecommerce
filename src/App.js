a931d3f3e7c4239abdc6be34ac13ce11c24885f9
Umair <devumairabid01@gmail.com>
2023-08-29 04:10:47 -0700

    part 4 : add product detail

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
 } from "react-router-dom";
 import Cart from './features/cart/Cart';
 import CartPage from './pages/CartPage';
+import CheckOut from './pages/CheckOut';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +30,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: (<CartPage></CartPage>),
+  },
+  {
+    path: "/checkout",
+    element: (<CheckOut></CheckOut>),
+  },
+  {
+    path: "/product-detail",
+    element: (<ProductDetailPage></ProductDetailPage>),
   },
 ]);
 
